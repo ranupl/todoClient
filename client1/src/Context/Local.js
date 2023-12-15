7c4179c8e4bc912480f00bf6b73eeedddf7696df
ranupl <ranupl542011@gmail.com>
2023-12-15 11:17:40 +0530

    build added

--- client1/src/Context/Local.js
+++ client1/src/Context/Local.js
@@ -1,3 +1,7 @@
-const Context = "http://localhost:5000";
+const isLocal = true;
+
+const Context = isLocal
+  ? "http://localhost:5000"
+  : "https://todo-980w.onrender.com";
 
 export default Context;
